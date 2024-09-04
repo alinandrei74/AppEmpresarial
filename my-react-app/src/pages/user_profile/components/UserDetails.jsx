ea93314f8b60e94c0fcf348531cffc02ad2a0ca0
Code Varied <codevaried@gmail.com>
2024-09-04 04:49:56 +0200

    UserDetails.jsx

--- my-react-app/src/pages/user_profile/components/UserDetails.jsx
+++ my-react-app/src/pages/user_profile/components/UserDetails.jsx
@@ -8,41 +8,69 @@ import "./UserDetails.css"; // Importa los estilos CSS
  * @returns {JSX.Element} UserDetails
  */
 const UserDetails = ({ userData }) => {
+  // Combina los nombres del usuario si están disponibles
+  const fullName = [
+    userData.personalData?.first_name,
+    userData.personalData?.middle_name,
+    userData.personalData?.last_name,
+  ]
+    .filter(Boolean) // Filtra los valores que no sean nulos o indefinidos
+    .join(" "); // Une los nombres con un espacio
+
   return (
     <div className="user-details-container">
       <div className="user-details-header">
         <h2>Información del Usuario</h2>
-        <div className={`user-role ${userData.role_name.toLowerCase()}`}>
-          {userData.role_name}
-        </div>
+        {userData.role_name && (
+          <div className={`user-role ${userData.role_name.toLowerCase()}`}>
+            {userData.role_name}
+          </div>
+        )}
       </div>
       <div className="user-info">
-        <div className="user-info-item">
-          <strong>Nombre de Usuario:</strong> {userData.username}
-        </div>
-        <div className="user-info-item">
-          <strong>Nombre Completo:</strong> {userData.personalData.first_name}{" "}
-          {userData.personalData.middle_name} {userData.personalData.last_name}
-        </div>
-        <div className="user-info-item">
-          <strong>DNI:</strong> {userData.additionalData.dni}
-        </div>
-        <div className="user-info-item">
-          <strong>Dirección:</strong> {userData.additionalData.address},{" "}
-          {userData.additionalData.postal_code}
-        </div>
+        {userData.username && (
+          <div className="user-info-item">
+            <strong>Nombre de Usuario:</strong> {userData.username}
+          </div>
+        )}
+        {/* Mostrar el nombre completo solo si alguno de los nombres existe */}
+        {fullName && (
+          <div className="user-info-item">
+            <strong>Nombre Completo:</strong> {fullName}
+          </div>
+        )}
+        {userData.additionalData && userData.additionalData.dni && (
+          <div className="user-info-item">
+            <strong>DNI:</strong> {userData.additionalData.dni}
+          </div>
+        )}
+        {userData.additionalData &&
+          userData.additionalData.address &&
+          userData.additionalData.postal_code && (
+            <div className="user-info-item">
+              <strong>Dirección:</strong> {userData.additionalData.address},{" "}
+              {userData.additionalData.postal_code}
+            </div>
+          )}
       </div>
-      <div className="user-contact">
-        <h3>Contacto</h3>
-        <ul>
-          <li>
-            <span>Email:</span> {userData.contactData.email}
-          </li>
-          <li>
-            <span>Teléfono:</span> {userData.contactData.phone_number}
-          </li>
-        </ul>
-      </div>
+      {userData.contactData &&
+        (userData.contactData.email || userData.contactData.phone_number) && (
+          <div className="user-contact">
+            <h3>Contacto</h3>
+            <ul>
+              {userData.contactData.email && (
+                <li>
+                  <span>Email:</span> {userData.contactData.email}
+                </li>
+              )}
+              {userData.contactData.phone_number && (
+                <li>
+                  <span>Teléfono:</span> {userData.contactData.phone_number}
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
     </div>
   );
 };
