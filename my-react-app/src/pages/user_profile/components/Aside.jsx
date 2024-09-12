902c3784f487e6d0db698b96b561ec81db566242
Code Varied <codevaried@gmail.com>
2024-09-12 22:48:26 +0200

    Update Aside

--- my-react-app/src/pages/user_profile/components/Aside.jsx
+++ my-react-app/src/pages/user_profile/components/Aside.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Aside.css";
 
 /**
@@ -8,6 +8,7 @@ import "./Aside.css";
  * @returns {JSX.Element} Aside
  */
 const Aside = () => {
+  const location = useLocation(); //; Hook para obtener la ubicación actual
   const menuItems = [
     { name: "Tasks", path: "tasks" },
     { name: "Notes", path: "notes" },
@@ -19,11 +20,28 @@ const Aside = () => {
     <aside className="aside">
       <div className="logo">Mi Aplicación</div>
       <ul className="aside-items">
-        {menuItems.map((item, index) => (
-          <li key={index}>
-            <Link to={item.path}>{item.name}</Link>
-          </li>
-        ))}
+        {menuItems.map((item, index) => {
+          //; Determinar si la ruta actual coincide con la ruta del elemento
+          const isActive = location.pathname.includes(item.path);
+          return (
+            <li key={index}>
+              <Link
+                to={item.path}
+                className={isActive ? "active-link" : ""}
+                style={{
+                  backgroundColor: isActive
+                    ? "var(--clr-OnSecondary)" //; Usar el color secundario oscuro para el elemento activo
+                    : "transparent",
+                  color: isActive
+                    ? "var(--clr-OnText)"
+                    : "var(--clr-OnBackground)",
+                }}
+              >
+                {item.name}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
